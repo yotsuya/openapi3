a611b45bb9207cdf4b50b96a8bd18a42ea745c01
Kenzo Yotsuya <1560182+yotsuya@users.noreply.github.com>
2021-02-05 22:56:58 +0900

    Add tests for `decode`

---
+++ inspection/Data/OpenApi/InternalSpec.hs
@@ -0,0 +1,121 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes       #-}
+
+module Data.OpenApi.InternalSpec (spec) where
+
+import           Data.Aeson            (decode)
+import           Data.ByteString.Lazy  (ByteString)
+import           Data.Maybe            (isJust)
+import           Data.String.Here      (here)
+import           Test.Hspec
+
+import           Data.OpenApi.Internal (OpenApi)
+
+
+decode' :: ByteString -> Maybe OpenApi
+decode' = decode
+
+openApiJson :: ByteString
+openApiJson = [here|
+{
+    "openapi": "3.0.0",
+    "info": { "title": "info example", "version": "1.0.0" },
+    "paths": {
+        "/pets": {
+            "post": {
+                "requestBody": {
+                    "content": {
+                        "application/json": {
+                            "schema": {
+                                "$ref": "#/components/schemas/Pet"
+                            }
+                        }
+                    }
+                },
+                "responses": { "default": { "description": "response example" } }
+            }
+        },
+        "/pets/{petId}": {
+            "parameters": [
+                {
+                    "name": "petId",
+                    "in": "path",
+                    "required": true,
+                    "schema": { "type": "integer" }
+                }
+            ],
+            "put": {
+                "requestBody": {
+                    "content": {
+                        "application/json": {
+                            "schema": {
+                                "$ref": "#/components/schemas/Pet"
+                            }
+                        }
+                    }
+                },
+                "responses": { "default": { "description": "response example" } }
+            }
+        }
+    },
+    "components": {
+        "schemas": {
+            "Pet": {
+                "type": "object",
+                "required": [ "name" ],
+                "properties": {
+                    "name": { "type": "string" },
+                    "age": { "type": "integer" }
+                }
+            }
+        }
+    }
+}
+|]
+
+spec :: Spec
+spec =
+    describe "decode" $ do
+        it "returns Just OpenApi if the OpenAPI document is valid" $
+            decode' openApiJson `shouldSatisfy` isJust
+
+        it "returns Nothing if the OpenAPI document is invalid" $
+            decode' "" `shouldBe` Nothing
+
+        -- TODO: report to openapi3 team
+        it "returns Just OpenApi even if the OpenAPI document has no components" $ do
+            let json = [here|
+{
+    "openapi": "3.0.0",
+    "info": { "title": "info example", "version": "1.0.0" },
+    "paths": {
+        "/pets": {
+            "post": {
+                "requestBody": {
+                    "content": {
+                        "application/json": {
+                            "schema": {
+                                "$ref": "#/components/schemas/Pet"
+                            }
+                        }
+                    }
+                },
+                "responses": { "default": { "description": "response example" } }
+            }
+        }
+    }
+}
+|]
+            decode' json `shouldSatisfy` isJust
+
+        -- TODO: report to openapi3 team
+        it "returns Nothing if the OpenAPI document has no Path objects" $ do
+            let json = [here|
+{
+    "openapi": "3.0.0",
+    "info": { "title": "info example", "version": "1.0.0" },
+    "components": {
+    }
+}
+|]
+            decode' json `shouldBe` Nothing
